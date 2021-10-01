f54a8ccfc25a1f208032c10deece41d4eb89dffd
DevExpressExampleBot <fakeEmail>
2021-10-01 16:40:09 +0300

    autogenerate VB

--- VB/DXGridDataPager/MainWindow.xaml.vb
+++ VB/DXGridDataPager/MainWindow.xaml.vb
@@ -1,95 +1,98 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Collections.ObjectModel
 Imports System.ComponentModel
 
 Namespace DXGridDataPager
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			DataContext = New VM()
-			InitializeComponent()
-		End Sub
-	End Class
-	Public Class VM
-		Implements INotifyPropertyChanged
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Event PropertyChanged As PropertyChangedEventHandler
-'INSTANT VB NOTE: The field pageIndex was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private pageIndex_Conflict As Integer = 1
-		Public Property PageIndex() As Integer
-			Get
-				Return pageIndex_Conflict
-			End Get
-			Set(ByVal value As Integer)
-				If value = pageIndex_Conflict Then
-					Return
-				End If
-				pageIndex_Conflict = value
-				OnPropertyChanged("PageIndex")
-			End Set
-		End Property
-		Protected Sub OnPropertyChanged(ByVal propertyName As String)
-			Dim handler As PropertyChangedEventHandler = PropertyChangedEvent
-			If handler IsNot Nothing Then
-				handler(Me, New PropertyChangedEventArgs(propertyName))
-			End If
-		End Sub
-		Private privatePagedCollection As List(Of TestDataList)
-		Public Property PagedCollection() As List(Of TestDataList)
-			Get
-				Return privatePagedCollection
-			End Get
-			Private Set(ByVal value As List(Of TestDataList))
-				privatePagedCollection = value
-			End Set
-		End Property
-		Public Sub New()
-			PagedCollection = New List(Of TestDataList)()
-			PagedCollection.Add(TestDataList.Create(0))
-			PagedCollection.Add(TestDataList.Create(3))
-			PagedCollection.Add(TestDataList.Create(5))
-			PagedCollection.Add(TestDataList.Create(7))
-			PageIndex = 2
-		End Sub
-	End Class
-	Public Class TestDataList
-		Inherits ObservableCollection(Of TestDataItem)
+        Public Sub New()
+            DataContext = New VM()
+            InitializeComponent()
+        End Sub
+    End Class
 
-		Public Shared Function Create(ByVal cc As Integer) As TestDataList
-			Dim res As New TestDataList()
-			For i As Integer = 0 To 9
-				Dim item As New TestDataItem()
-				item.ID = i
-				item.Value = (ChrW(CInt(Math.Truncate(AscW("A"c))) + cc)).ToString()
-				res.Add(item)
-			Next i
-			For i As Integer = 0 To 9
-				Dim item As New TestDataItem()
-				item.ID = i
-				item.Value = (ChrW(CInt(Math.Truncate(AscW("B"c))) + cc)).ToString()
-				res.Add(item)
-			Next i
-			Return res
-		End Function
-	End Class
-	Public Class TestDataItem
-		Public Property ID() As Integer
-		Public Property Value() As String
-	End Class
+    Public Class VM
+        Implements INotifyPropertyChanged
+
+        Private _PagedCollection As List(Of DXGridDataPager.TestDataList)
+
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Private pageIndexField As Integer = 1
+
+        Public Property PageIndex As Integer
+            Get
+                Return pageIndexField
+            End Get
+
+            Set(ByVal value As Integer)
+                If value = pageIndexField Then Return
+                pageIndexField = value
+                OnPropertyChanged("PageIndex")
+            End Set
+        End Property
+
+        Protected Sub OnPropertyChanged(ByVal propertyName As String)
+            Dim handler As PropertyChangedEventHandler = PropertyChangedEvent
+            If handler IsNot Nothing Then
+                handler(Me, New PropertyChangedEventArgs(propertyName))
+            End If
+        End Sub
+
+        Public Property PagedCollection As List(Of TestDataList)
+            Get
+                Return _PagedCollection
+            End Get
+
+            Private Set(ByVal value As List(Of TestDataList))
+                _PagedCollection = value
+            End Set
+        End Property
+
+        Public Sub New()
+            PagedCollection = New List(Of TestDataList)()
+            PagedCollection.Add(TestDataList.Create(0))
+            PagedCollection.Add(TestDataList.Create(3))
+            PagedCollection.Add(TestDataList.Create(5))
+            PagedCollection.Add(TestDataList.Create(7))
+            PageIndex = 2
+        End Sub
+    End Class
+
+    Public Class TestDataList
+        Inherits ObservableCollection(Of TestDataItem)
+
+        Public Shared Function Create(ByVal cc As Integer) As TestDataList
+            Dim res As TestDataList = New TestDataList()
+            For i As Integer = 0 To 10 - 1
+                Dim item As TestDataItem = New TestDataItem()
+                item.ID = i
+                item.Value = Microsoft.VisualBasic.ChrW(Microsoft.VisualBasic.AscW("A"c) + cc).ToString()
+                res.Add(item)
+            Next
+
+            For i As Integer = 0 To 10 - 1
+                Dim item As TestDataItem = New TestDataItem()
+                item.ID = i
+                item.Value = Microsoft.VisualBasic.ChrW(Microsoft.VisualBasic.AscW("B"c) + cc).ToString()
+                res.Add(item)
+            Next
+
+            Return res
+        End Function
+    End Class
+
+    Public Class TestDataItem
+
+        Public Property ID As Integer
+
+        Public Property Value As String
+    End Class
 End Namespace
--- VB/DXGridDataPager/SourcesBehavior.vb
+++ VB/DXGridDataPager/SourcesBehavior.vb
@@ -1,109 +1,107 @@
-﻿Imports System.Windows.Interactivity
+Imports System.Windows.Interactivity
 Imports DevExpress.Xpf.Editors.DataPager
 Imports System.Windows
 Imports System.Collections
 Imports System.Collections.Specialized
+
 Namespace DXGridDataPager
-	Public Class SourcesBehavior
-		Inherits Behavior(Of DataPager)
 
-		Public Shared ReadOnly ActualSourceProperty As DependencyProperty = DependencyProperty.Register("ActualSource", GetType(Object), GetType(SourcesBehavior), Nothing)
-		Public Shared ReadOnly SourcesProperty As DependencyProperty = DependencyProperty.Register("Sources", GetType(IList), GetType(SourcesBehavior), New PropertyMetadata(Nothing, Sub(d, e) (CType(d, SourcesBehavior)).OnSourcesChanged(DirectCast(e.NewValue, IList))))
-		Public Property ActualSource() As Object
-			Get
-				Return DirectCast(GetValue(ActualSourceProperty), Object)
-			End Get
-			Set(ByVal value As Object)
-				SetValue(ActualSourceProperty, value)
-			End Set
-		End Property
-		Public Property Sources() As IList
-			Get
-				Return DirectCast(GetValue(SourcesProperty), IList)
-			End Get
-			Set(ByVal value As IList)
-				SetValue(SourcesProperty, value)
-			End Set
-		End Property
-		Public ReadOnly Property DataPager() As DataPager
-			Get
-				Return AssociatedObject
-			End Get
-		End Property
-		Protected Overrides Sub OnAttached()
-			MyBase.OnAttached()
-			'DataPager.PageSize = Sources.Count;
-			DataPager.ItemCount = Sources.Count
-			'DataPager.PageSize = Sou
-			UpdateActualSource(DataPager.PageIndex)
-			SubsribeSourcesColletion(Sources)
-			SubscribeDataPager()
-		End Sub
-		Protected Overrides Sub OnDetaching()
-			UnsubsribeDataPager()
-			UnsubsribeSourcesColletion(Sources)
-			MyBase.OnDetaching()
-		End Sub
-		Private Sub OnSourcesChanged(ByVal oldSources As IList)
-			If DataPager Is Nothing Then
-				Return
-			End If
-			UpdateActualSource(DataPager.PageIndex)
-			If Sources IsNot Nothing Then
-				DataPager.ItemCount = Sources.Count
-			End If
-			UnsubsribeSourcesColletion(oldSources)
-			SubsribeSourcesColletion(Sources)
-			If ActualSource IsNot Nothing Then
-				DataPager.PageIndex = Sources.IndexOf(ActualSource)
-			End If
-			SubscribeDataPager()
-		End Sub
-		Private Sub UnsubsribeSourcesColletion(ByVal sources As IList)
-			If TypeOf sources Is INotifyCollectionChanged Then
-				RemoveHandler DirectCast(sources, INotifyCollectionChanged).CollectionChanged, AddressOf Sources_CollectionChanged
-			End If
-		End Sub
-		Private Sub SubsribeSourcesColletion(ByVal sources As IList)
-			UnsubsribeSourcesColletion(sources)
-			If TypeOf sources Is INotifyCollectionChanged Then
-				AddHandler DirectCast(sources, INotifyCollectionChanged).CollectionChanged, AddressOf Sources_CollectionChanged
-			End If
-		End Sub
-		Private Sub UnsubsribeDataPager()
-			If DataPager Is Nothing Then
-				Return
-			End If
-			RemoveHandler DataPager.PageIndexChanged, AddressOf DataPager_PageIndexChanged
-		End Sub
-		Private Sub SubscribeDataPager()
-			If DataPager Is Nothing Then
-				Return
-			End If
-			UnsubsribeDataPager()
-			AddHandler DataPager.PageIndexChanged, AddressOf DataPager_PageIndexChanged
-		End Sub
+    Public Class SourcesBehavior
+        Inherits Behavior(Of DataPager)
 
-		Private Sub Sources_CollectionChanged(ByVal sender As Object, ByVal e As System.Collections.Specialized.NotifyCollectionChangedEventArgs)
-			If Sources IsNot Nothing Then
-				DataPager.ItemCount = Sources.Count
-			End If
-			If ActualSource Is Nothing Then
-				UpdateActualSource(DataPager.PageIndex)
-			End If
-		End Sub
-		Private Sub UpdateActualSource(ByVal index As Integer)
-			If Sources IsNot Nothing AndAlso Sources.Count > 0 Then
-				If index < Sources.Count Then
-					ActualSource = Sources(index)
-				Else
-					ActualSource = Sources(0)
-				End If
-			End If
+        Public Shared ReadOnly ActualSourceProperty As DependencyProperty = DependencyProperty.Register("ActualSource", GetType(Object), GetType(SourcesBehavior), Nothing)
 
-		End Sub
-		Private Sub DataPager_PageIndexChanged(ByVal sender As Object, ByVal e As DataPagerPageIndexChangedEventArgs)
-			UpdateActualSource(e.NewValue)
-		End Sub
-	End Class
+        Public Shared ReadOnly SourcesProperty As DependencyProperty = DependencyProperty.Register("Sources", GetType(IList), GetType(SourcesBehavior), New PropertyMetadata(Nothing, Sub(d, e) CType(d, SourcesBehavior).OnSourcesChanged(CType(e.NewValue, IList))))
+
+        Public Property ActualSource As Object
+            Get
+                Return CObj(GetValue(ActualSourceProperty))
+            End Get
+
+            Set(ByVal value As Object)
+                SetValue(ActualSourceProperty, value)
+            End Set
+        End Property
+
+        Public Property Sources As IList
+            Get
+                Return CType(GetValue(SourcesProperty), IList)
+            End Get
+
+            Set(ByVal value As IList)
+                SetValue(SourcesProperty, value)
+            End Set
+        End Property
+
+        Public ReadOnly Property DataPager As DataPager
+            Get
+                Return AssociatedObject
+            End Get
+        End Property
+
+        Protected Overrides Sub OnAttached()
+            MyBase.OnAttached()
+            'DataPager.PageSize = Sources.Count;
+            DataPager.ItemCount = Sources.Count
+            'DataPager.PageSize = Sou
+            UpdateActualSource(DataPager.PageIndex)
+            SubsribeSourcesColletion(Sources)
+            SubscribeDataPager()
+        End Sub
+
+        Protected Overrides Sub OnDetaching()
+            UnsubsribeDataPager()
+            UnsubsribeSourcesColletion(Sources)
+            MyBase.OnDetaching()
+        End Sub
+
+        Private Sub OnSourcesChanged(ByVal oldSources As IList)
+            If DataPager Is Nothing Then Return
+            UpdateActualSource(DataPager.PageIndex)
+            If Sources IsNot Nothing Then DataPager.ItemCount = Sources.Count
+            UnsubsribeSourcesColletion(oldSources)
+            SubsribeSourcesColletion(Sources)
+            If ActualSource IsNot Nothing Then DataPager.PageIndex = Sources.IndexOf(ActualSource)
+            SubscribeDataPager()
+        End Sub
+
+        Private Sub UnsubsribeSourcesColletion(ByVal sources As IList)
+            If TypeOf sources Is INotifyCollectionChanged Then RemoveHandler CType(sources, INotifyCollectionChanged).CollectionChanged, AddressOf Sources_CollectionChanged
+        End Sub
+
+        Private Sub SubsribeSourcesColletion(ByVal sources As IList)
+            UnsubsribeSourcesColletion(sources)
+            If TypeOf sources Is INotifyCollectionChanged Then AddHandler CType(sources, INotifyCollectionChanged).CollectionChanged, AddressOf Sources_CollectionChanged
+        End Sub
+
+        Private Sub UnsubsribeDataPager()
+            If DataPager Is Nothing Then Return
+            Me.DataPager.PageIndexChanged -= AddressOf DataPager_PageIndexChanged
+        End Sub
+
+        Private Sub SubscribeDataPager()
+            If DataPager Is Nothing Then Return
+            UnsubsribeDataPager()
+            Me.DataPager.PageIndexChanged += AddressOf DataPager_PageIndexChanged
+        End Sub
+
+        Private Sub Sources_CollectionChanged(ByVal sender As Object, ByVal e As NotifyCollectionChangedEventArgs)
+            If Sources IsNot Nothing Then DataPager.ItemCount = Sources.Count
+            If ActualSource Is Nothing Then UpdateActualSource(DataPager.PageIndex)
+        End Sub
+
+        Private Sub UpdateActualSource(ByVal index As Integer)
+            If Sources IsNot Nothing AndAlso Sources.Count > 0 Then
+                If index < Sources.Count Then
+                    ActualSource = Sources(index)
+                Else
+                    ActualSource = Sources(0)
+                End If
+            End If
+        End Sub
+
+        Private Sub DataPager_PageIndexChanged(ByVal sender As Object, ByVal e As DataPagerPageIndexChangedEventArgs)
+            UpdateActualSource(e.NewValue)
+        End Sub
+    End Class
 End Namespace
