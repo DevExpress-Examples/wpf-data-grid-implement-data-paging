7fb74db0b19892db50f70c150d890a90d9853aee
AndreySlabov <andrey.slabov@devexpress.com>
2023-01-09 12:57:07 +0400

    Update Sample

--- CS/DXGridDataPager/MainWindow.xaml.cs
+++ CS/DXGridDataPager/MainWindow.xaml.cs
@@ -1,72 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using DevExpress.Mvvm;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System.Windows;
 
 namespace DXGridDataPager {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
-            DataContext = new VM();
             InitializeComponent();
         }
     }
-    public class VM : INotifyPropertyChanged {
-        public event PropertyChangedEventHandler PropertyChanged;
-        int pageIndex = 1;
-        public int PageIndex {
-            get { return pageIndex; }
-            set {
-                if(value == pageIndex) return;
-                pageIndex = value;
-                OnPropertyChanged("PageIndex");
-            }
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            Source = TestDataList.CreateData();
         }
-        protected void OnPropertyChanged(string propertyName) {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if(handler != null) {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        public List<TestDataList> PagedCollection { get; private set; }
-        public VM() {
-            PagedCollection = new List<TestDataList>();
-            PagedCollection.Add(TestDataList.Create(0));
-            PagedCollection.Add(TestDataList.Create(3));
-            PagedCollection.Add(TestDataList.Create(5));
-            PagedCollection.Add(TestDataList.Create(7));
-            PageIndex = 2;
-        }
+        public ObservableCollection<TestDataItem> Source { get; set; }
     }
-    public class TestDataList : ObservableCollection<TestDataItem> {
-        public static TestDataList Create(int cc) {
-            TestDataList res = new TestDataList();
+    public class TestDataList {
+        public static ObservableCollection<TestDataItem> CreateData() {
+            var testData = new ObservableCollection<TestDataItem>();
             for(int i = 0; i < 10; i++) {
-                TestDataItem item = new TestDataItem();
-                item.ID = i;
-                item.Value = ((char)((int)'A' + cc)).ToString();
-                res.Add(item);
+                for (int j = 0; j < 10; j++) {
+                    TestDataItem item = new TestDataItem {
+                        ID = i * 10 + j,
+                        Value = ((char)((int)'A' + i)).ToString()
+                    };
+                    testData.Add(item);
+                }
             }
-            for(int i = 0; i < 10; i++) {
-                TestDataItem item = new TestDataItem();
-                item.ID = i;
-                item.Value = ((char)((int)'B' + cc)).ToString();
-                res.Add(item);
-            }
-            return res;
+            return testData;
         }
     }
     public class TestDataItem {
--- Readme.md
+++ Readme.md
@@ -3,25 +3,24 @@
 [![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)](https://supportcenter.devexpress.com/ticket/details/E4114)
 [![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
 <!-- default badges end -->
-<!-- default file list -->
-*Files to look at*:
 
-* [MainWindow.xaml](./CS/DXGridDataPager/MainWindow.xaml) (VB: [MainWindow.xaml](./VB/DXGridDataPager/MainWindow.xaml))
-* [MainWindow.xaml.cs](./CS/DXGridDataPager/MainWindow.xaml.cs) (VB: [MainWindow.xaml.vb](./VB/DXGridDataPager/MainWindow.xaml.vb))
-* [SourcesBehavior.cs](./CS/DXGridDataPager/SourcesBehavior.cs) (VB: [SourcesBehavior.vb](./VB/DXGridDataPager/SourcesBehavior.vb))
-<!-- default file list end -->
-# [Obsolete] How to implement data paging
+# WPF Data Grid - Implement Data Paging
 
 Silverlight supports the data paging mechanism natively. In the meantime, there are no easy approaches to implementing the **IPagedCollectionView** interface in WPF.
-
   
 We supported **Data Paging** in **v18.1** and later versions. Please refer to [Data Paging](https://docs.devexpress.com/WPF/120186/controls-and-libraries/data-grid/paging-and-scrolling/data-paging) for more information.  
   
 The following solution is applicable to the earlier versions:  
 The easiest way to implement data paging is to change the grid's ItemsSource when the page index is changed. This example contains special attached behavior that allows you to enable this feature with ease.
 
-  
-**See Also:**  
-[How to support paging in DXGrid by implementing the IPagedCollectionView interface](https://www.devexpress.com/Support/Center/p/T226182.aspx)
+## Files to Review
 
+* [MainWindow.xaml](./CS/DXGridDataPager/MainWindow.xaml) (VB: [MainWindow.xaml](./VB/DXGridDataPager/MainWindow.xaml))
+* [MainWindow.xaml.cs](./CS/DXGridDataPager/MainWindow.xaml.cs) (VB: [MainWindow.xaml.vb](./VB/DXGridDataPager/MainWindow.xaml.vb))
 
+## Documentation
+
+* [Data Paging](https://docs.devexpress.com/WPF/120186/controls-and-libraries/data-grid/paging-and-scrolling/data-paging)
+* [GridViewBase.AllowPaging](https://docs.devexpress.com/WPF/DevExpress.Xpf.Grid.GridViewBase.AllowPaging)
+
+## More Examples
